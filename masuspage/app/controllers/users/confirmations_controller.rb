1043a30b542126c59ee91ccf9231724b94b6b48b
PatrykPaszenda <patrykpaszenda@interia.eu>
2018-02-06 19:46:56 +0000

    The most recent version

--- masuspage/app/controllers/users/confirmations_controller.rb
+++ masuspage/app/controllers/users/confirmations_controller.rb
@@ -1,15 +1,15 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
-#  GET /resource/confirmation/new
+ # GET /resource/confirmation/new
   def new
     super
   end
 
-#  POST /resource/confirmation
+ # POST /resource/confirmation
   def create
     super
   end
 
-#  GET /resource/confirmation?confirmation_token=abcdef
+ # GET /resource/confirmation?confirmation_token=abcdef
   def show
     super
   end
@@ -25,4 +25,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, resource)
     super(resource_name, resource)
   end
+  def index
+  if params[:approved] == "false"
+    @users = User.where(approved: false)
+  else
+    @users = User.all
+  end
+end
 end
--- masuspage/config/routes.rb
+++ masuspage/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
       Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
         devise_for :users, controllers: {
           sessions: 'users/sessions'
         }
